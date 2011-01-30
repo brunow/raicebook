f36a74427970f5eea0cefe5b85384b56032a62fb
Bruno Wernimont <hello@brunowernimont.be>
2011-01-30 17:00:10 +0100

    forgot to add lib/raicebook

--- lib/raicebook.rb
+++ lib/raicebook.rb
@@ -1,6 +1,8 @@
 require 'raicebook'
+require 'raicebook/base'
 require 'raicebook/helpers'
 require "action_controller"
+require 'raicebook/fb_events'
 
 ActiveSupport.on_load(:action_view) do
   include Raicebook::Helpers
